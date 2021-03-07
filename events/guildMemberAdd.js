b63a173f622ac2f761d5a269b14af4110f9d7c0a
dbr <dbr@dukebr.com>
2021-03-07 12:50:21 -0300

    Update guildMemberAdd.js
    Atualizado para v12

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,22 +1,13 @@
-module.exports = async (client, member) => {
-	
-	const embed = {
-	  "color": 8311585,
-	  "timestamp": new Date(),
-	  "footer": {
-		"icon_url": client.user.avatarURL,
-		"text": "Data de entrada"
-	  },
-	  "author": {
-		"name": member.user.username,
-		"icon_url": member.user.avatarURL
-	  },
-	  "fields": [
-		{
-		  "name": "Bem vindo(a)",
-		  "value": "Leia as <#464522380849709056> para não tomar KICK/BAN e mantenha um bom relacionamento com o pessoal :sunglasses: "
-		}
-	  ]
-	};
-	member.guild.channels.get("464520117770780678").send({embed});
+module.exports = async (client, member) =>{
+	const Discord = require("discord.js");
+
+	const embed = new Discord.MessageEmbed()
+	.setTitle(member.user.username + " Bem vindo(a) ao servidor!")
+	.setAuthor(client.user.username, client.user.displayAvatarURL())
+	.setImage(member.user.avatarURL())
+	.setColor('#ffv800')
+	.setDescription('Leia as <#ID_DO_CANAL_DE_REGRAS> antes de postar!')
+	.setTimestamp(new Date())
+
+	member.guild.channels.cache.get('ID_DO_CANAL_PARA_MENSAGEM').send({embed});
 }
